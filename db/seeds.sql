4e644a8b4065e1f6e7d6e38c4a80da9b9c31847a
Dingus <35214896+EddieG77@users.noreply.github.com>
2018-08-15 17:57:35 -0500

    push

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Breakfast Burger', 0);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Bacon Cheddar Burger', 0);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Portobello Mushroom Burger', 1);
